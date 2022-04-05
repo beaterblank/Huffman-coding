97b6c802c6dc6265a700c3e30dcc50358a6e4073
G.Mohan Teja <mohanteja.g2019@vitstudent.ac.in>
2022-04-05 22:00:26 +0530

    Update hashtable.c

--- code/hashtable.c
+++ code/hashtable.c
@@ -1,108 +1,11 @@
-
-
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdbool.h>
-
 #define SIZE 256
-
-struct node
+int freq[SIZE];
+void insert(char str[])
 {
-    unsigned short data;
-    struct node *next;
-};
-
-struct node *chain[SIZE];
-
-void init()
-{
-    int i;
-    for(i = 0; i < SIZE; i++)
-        chain[i] = NULL;
-}
-
-void insert(int value)
-{
-    //create a newnode with value
-    struct node *newNode = malloc(sizeof(struct node));
-    newNode->data = value;
-    newNode->next = NULL;
-
-    //calculate hash key
-    int key = value % SIZE;
-
-    //check if chain[key] is empty
-    if(chain[key] == NULL)
-        chain[key] = newNode;
-    //collision
-    else
-    {
-        //add the node at the end of chain[key].
-        struct node *temp = chain[key];
-        while(temp->next)
-        {
-            temp = temp->next;
-        }
-
-        temp->next = newNode;
-    }
-}
-
-void print()
-{
-    int i;
-
-    for(i = 0; i < SIZE; i++)
-    {
-        struct node *temp = chain[i];
-        if (temp == NULL)
-        {
-            continue;
-        }
-        else
-        {
-            printf("chain[%d]-->",i);
-            while(temp)
-            {
-                printf("%d -->",temp->data);
-                temp = temp->next;
-            }
-            printf("NULL\n");
-        }
-    }
-}
-
-int count(char a)
-{
-    int i = (int)a;
-    struct node *temp = chain[i];
-    int count = 0;
-    while (temp)
-    {
-        count++;
-        temp = temp->next;
-    }
-    return count;
-}
-
-int main()
-{
-    //init array of list to NULL
-    init();
-
-    insert('a');
-    insert('a');
-    insert('b');
-    insert('c');
-    insert('d');
-    insert('e');
-    insert('f');
-    printf("%d\n",count('a'));
-    printf("%d\n",count('f'));
-    printf("%d\n",count('z'));
-
-    print();
-
-    return 0;
+    // size of the string 'str'
+    int n = strlen(str);
+    // initialize all elements of freq[] to 0
+    memset(freq, 0, sizeof(freq));
+    // accumulate frequency of each character in 'str'
+    for (int i = 0; i < n; i++)freq[(int)str[i]]=freq[(int)str[i]]+1;
 }
